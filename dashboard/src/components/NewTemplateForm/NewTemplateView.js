deb4ec8890cc1cceaccf5523503860b7dfa48d86
nihilok <mjfullstack@gmail.com>
2021-06-23 16:14:45 +0100

    help

--- dashboard/src/components/NewTemplateForm/NewTemplateView.js
+++ dashboard/src/components/NewTemplateForm/NewTemplateView.js
@@ -10,7 +10,7 @@ const NewTemplateView = ({getTemplates}) => {
         <h3>Upload Template</h3>
         <NewTemplateFormComponent getTemplates={getTemplates || undefined}/>
         {showHelp ?
-            <div style={{margin: '1rem 0 0 0'}}><small>Templates should be in the form of a .docx file, formatted as you would like the final document to be. Variables are declared between double {'{{}}'}</small>
+            <div style={{margin: '1rem 0 0 0'}}><small>Templates should be in the form of a .docx file, formatted as you would like the final document to be. Variables with distinct names should be declared between double {'{{}}'}</small>
               <p>e.g: <code>{'{{variable}}'}</code></p>
               <small>To specify that a variable should represent 1 or more paragraphs you should prefix the variable with
                 "__para_"</small> <p>e.g: <code>{'{{__para_variable}}'}</code></p></div> : ''}
