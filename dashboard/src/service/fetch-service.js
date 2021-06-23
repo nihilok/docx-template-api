2c106f6da2e9cb54063f87e5b93ae1cdd9c4615d
nihilok <mjfullstack@gmail.com>
2021-06-23 14:38:11 +0100

    fetch

--- dashboard/src/service/fetch-service.js
+++ dashboard/src/service/fetch-service.js
@@ -65,8 +65,6 @@ export const FetchAuth = async (
     method: 'POST',
     body: formData
   }).then((response) => {
-      console.log(response)
-
     if (response.status !== 200) {
       throw new Error(`Bad response: ${response.status}`)
     }
