042b0654cc43fda61d4e172d64ed906e5d597c2a
nihilok <mjfullstack@gmail.com>
2021-06-24 21:53:40 +0100

    help text

--- dashboard/src/components/NewTemplateForm/NewTemplateView.js
+++ dashboard/src/components/NewTemplateForm/NewTemplateView.js
@@ -13,12 +13,13 @@ const NewTemplateView = ({getTemplates}) => {
             <div style={{margin: '1rem 0 0 0'}} className="container help"><small>Templates should be in the form of a .docx file, formatted as you
               would like the final document to be. Variables with distinct names should be declared between
               double {'{{}}'}</small>
-              <p>e.g: <code>{'{{variable1}}'} some other text {'{{variable2}}'}</code></p>
+              <p>e.g: <code>My name is {'{{name}}'} and I'm {'{{age}}'} years old.</code></p>
               <small>To specify that a variable should represent 1 or more paragraphs you should prefix the variable
                 with
                 "__para_"</small> <p>e.g: <code>{'{{__para_variable}}'}</code></p>
               <small>Variables prefixed with "time_" will render a time input in the form.</small>
             <p>e.g: <code>{'{{time_of_appointment}}'}</code></p>
+              <small>Variable names <code>{"{{time}}, {{year}}, {{month}}, & {{date}}"}</code> are reserved, and will return HH:MM, YYYY, MM, & DD respectively at the time of rendering.</small>
             </div> : ''}
         <div className="form-group" style={{margin: '1rem'}}><a href="#" type="submit" onClick={() => setShowHelp(!showHelp)}
                                                                     >{showHelp ? 'Hide Help' : 'Show Help'}</a></div>
